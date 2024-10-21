8c747fef9ccb4e5cd8188c9bdb368241efa8be4c
田畑修一 <shuichi@macbookpro.local>
2024-10-21 15:12:59 +0900

    update access commit

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  //baseURL: "http://localhost:8080/api",
   //baseURL: "http://localhost:8090/api",
-  //baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
